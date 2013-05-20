aef432ca1c7f671482b73fa644a474a05293b0f9
Loïc Horellou <loic@formidable-studio.net>
2013-05-20 20:41:15 +0200

    upload files

---
+++ test1/server.js
@@ -0,0 +1,76 @@
+var app = require('http').createServer(handler)
+, io = require('socket.io').listen(app)
+, fs = require('fs')
+
+
+/*var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);*/
+
+io.set('log level', 2);
+app.listen(8333);
+
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/public/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+/*
+app.get('/', function (req, res) { 
+    res.sendfile(__dirname + '/index.html'); 
+}); 
+*/
+
+var allClients = 0; 
+var clientId = 1; 
+
+io.sockets.on('connection', function (client) { 
+    var my_timer; 
+    var my_client = { 
+        "id": clientId, 
+        "obj": client 
+    }; 
+    
+    clientId += 1; 
+    allClients += 1; 
+    
+    /*my_timer = setInterval(function () { 
+        my_client.obj.send(JSON.stringify({ 
+            "timestamp": (new Date()).getTime(), 
+            "clients": allClients 
+        })); 
+    }, 1000); 
+    */
+    client.on('message', function(data) {
+        if(data=='Poke'){
+            my_client.obj.broadcast.send(JSON.stringify({ 
+                poke: "poke send by client " + my_client.id 
+            }));
+        }else{
+            my_client.obj.broadcast.send(JSON.stringify({ 
+                message: "message sms sent by client " + my_client.id,
+                data: data
+            }));
+
+        }
+        console.log(data); 
+    });
+    
+    client.on('disconnect', function() { 
+        clearTimeout(my_timer); 
+        allClients -= 1; 
+        console.log('disconnect'); 
+    });
+
+});
